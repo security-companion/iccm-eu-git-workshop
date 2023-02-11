79159c16a1452b7ed8ec42c1e9c62179d0bc31cd
security-companion <mail@security-companion.net>
2023-02-11 17:19:19 +0100

    fix linting

--- calculator.py
+++ calculator.py
@@ -2,15 +2,15 @@
 
 import os
 
-def main():
+def  main():
 
     total_number = 0
-     
+
     for file_name in os.listdir("input-files"):
         if file_name.endswith(".txt"):
             print("Reading content of file {}".format(file_name))
 
-            f = open("./input-files/" + file_name,"r")
+            f = open("./input-files/" + file_name, "r")
             lines = f.readlines()
 
             for line in lines:
@@ -22,5 +22,5 @@ def main():
     print("Summary of all numbers in all files is: {}".format(total_number))
     print("*" * 60 + "\n")
 
-if __name__ == "__main__":
+if  __name__ == "__main__":
     main()
